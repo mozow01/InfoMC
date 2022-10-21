33348a11eb48fe29d4202d4ebef42722fb7d0e40
mozow01 <78300414+mozow01@users.noreply.github.com>
2022-10-21 18:55:54 +0200

    Add files via upload

---
+++ z_Git_and_GitHub_tutorial/myfirst.js
@@ -0,0 +1,19 @@
+/*
+sudo apt install node-typescript
+tsc --version
+cd Dokumentumok...
+tsc --init
+tsc myfirst.ts
+*/
+var üzenet = 'Hello World';
+console.log(üzenet);
+function helloPerson2(name) {
+    console.log("hello " + name);
+    return "done";
+}
+/*
+         "done".charAt(1);
+         helloPerson().charAt(1);
+         Math.abs(12);
+         Math.abs(helloPerson2("Peter"));
+*/ 
---
+++ z_Git_and_GitHub_tutorial/myfirst.ts
@@ -0,0 +1,23 @@
+ /*
+ sudo apt install node-typescript
+ tsc --version
+ cd Dokumentumok...
+ tsc --init
+ tsc myfirst.ts
+ */
+ 
+let üzenet : string = 'Hello World';
+        console.log(üzenet);
+
+function helloPerson2(name: string) 
+        {
+            console.log("hello " + name);
+            return "done"; 
+        }
+
+ /*       
+          "done".charAt(1);
+          helloPerson().charAt(1);
+          Math.abs(12);
+          Math.abs(helloPerson2("Peter")); 
+*/
