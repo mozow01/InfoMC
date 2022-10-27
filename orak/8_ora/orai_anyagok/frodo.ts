3dbe7e2742ed41b8e2de76a37def115e0b3df212
mozow01 <78300414+mozow01@users.noreply.github.com>
2022-10-27 11:59:40 +0200

    Add files via upload

---
+++ orak/8_ora/orai_anyagok/frodo.ts
@@ -0,0 +1,25 @@
+function elsoNKeplettel(input: number): number 
+{
+    return (input * (input + 1)) / 2;
+}
+    
+function elsoNRekurzivan(input: number): number {
+    if (input < 0) 
+    { return 0} 
+    else 
+    { return elsoNRekurzivan(input - 1) + input};
+}
+
+function elsoNForLooppal(input: number): number {
+    let osszeg: number = 0;
+    for (let index: number = 0; index < input + 1; index++) {
+        osszeg = osszeg + index ;
+        
+    }
+    { return osszeg};
+}
+
+function tetel(input: number): boolean {
+    let output: boolean = (elsoNRekurzivan(input) == elsoNKeplettel(input)); 
+    { return output};
+}
